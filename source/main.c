5570dbfa56300491818d7c928df1a7e0347d4c3d
TamirRothschild <81330776+TamirRothschild@users.noreply.github.com>
2024-06-03 15:15:48 +0300

    Update main.c

--- source/main.c
+++ source/main.c
@@ -85,7 +85,7 @@ int main(int argc, char *argv[]) {
     gtk_init(&argc, &argv);
 
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Text File Translator");
+    gtk_window_set_title(GTK_WINDOW(window), "QuickLing");
     gtk_container_set_border_width(GTK_CONTAINER(window), 10);
     gtk_window_set_default_size(GTK_WINDOW(window), 400, 200);
 
